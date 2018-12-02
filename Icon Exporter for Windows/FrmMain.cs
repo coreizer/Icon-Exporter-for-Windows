1c20bf865567c348c8e3edcf6476390cb6a4aea4
AlphaNyne <alphanyne@outlook.com>
2018-12-03 04:39:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Icon Exporter for Windows/FrmMain.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright (C) 2018 AlphaNyne
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace IconExporter
+{
+  public partial class FrmMain : Form
+  {
+    public FrmMain()
+    {
+      InitializeComponent();
+    }
+
+    private void ExportIcons(string fileName)
+    {
+      SaveFileDialog SFD = new SaveFileDialog() {
+        Filter = "PNG (*.png)|*.png",
+        FileName = $"{DateTime.Now.Ticks}-icon.png"
+      };
+
+      if (SFD.ShowDialog(this) == DialogResult.OK) {
+        Icon export = Icon.ExtractAssociatedIcon(fileName);
+        Bitmap icon = new Bitmap(export.ToBitmap());
+        icon.Save(SFD.FileName, ImageFormat.Png);
+        icon.Dispose();
+      }
+    }
+
+    private void ButtonExport_Click(object sender, EventArgs e)
+    {
+      try {
+        OpenFileDialog OFD = new OpenFileDialog() {
+          Filter = "Executable files (*.exe)|*.exe|*.All files|*.*",
+          RestoreDirectory = false,
+        };
+
+        if (OFD.ShowDialog(this) == DialogResult.OK) {
+          ExportIcons(OFD.FileName);
+        }
+      }
+      catch (Exception ex) {
+        MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+  }
+}
---
+++ Icon Exporter for Windows/frmMain.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace IconExporter
+{
+	partial class FrmMain
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(21, 26);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(260, 33);
+            this.buttonExport.TabIndex = 1;
+            this.buttonExport.Text = "開く...";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.ButtonExport_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 86);
+            this.Controls.Add(this.buttonExport);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Icon Exporter for Windows";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonExport;
+	}
+}
+
